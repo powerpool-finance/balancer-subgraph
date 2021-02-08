92da036eb75a35811515052912d0e589aeb8bebe
Poli Paul <polipaul777@hotmail.com>
2021-02-08 09:39:16 +0200

    Add a new factory & ASSY

--- src/mappings/oracle.ts
+++ src/mappings/oracle.ts
@@ -18,4 +18,6 @@ export function handleUnindexedUpdate(event: AnchorPriceUpdated): void {
   updatePool("0x26607ac599266b21d13c7acf7942c7701a8b699c", event);
   // YETI - 0xb4bebd34f6daafd808f73de0d10235a92fbb6c3d
   updatePool("0xb4bebd34f6daafd808f73de0d10235a92fbb6c3d", event);
+  // ASSY - 0xfa2562da1bba7b954f26c74725df51fb62646313
+  updatePool("0xfa2562da1bba7b954f26c74725df51fb62646313", event);
 }
